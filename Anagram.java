6c5be0d6503aacc1a763518ee463f93624ab6e7a
Sai Charan <saicharankodam1234@gmail.com>
2024-02-25 15:58:48 +0530

    First Commit

---
+++ Anagram.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+class Anagram
+{
+    public static void main(String[] args) {
+        String s1="A Gentle man";
+        String s2="Elegant man";
+        s2=s2.replace(" ","").toLowerCase();
+        s1=s1.replace(" ","").toLowerCase();
+        char[] c1=s2.toCharArray();
+        char[] c2=s1.toCharArray();
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+        if(Arrays.equals(c1,c2))
+        {
+            System.out.println("Anagram");
+        }
+        else{
+            System.out.println("Not Anagram");
+        }
+    }
+}
